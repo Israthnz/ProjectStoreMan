b2d77365de756b93148a8e902c84b32b0cf3abca
Israthnz <zahirul.israth@gmail.com>
2024-10-19 11:15:39 +0530

    Editted

--- src/com/storeman/app/MainApp.java
+++ src/com/storeman/app/MainApp.java
@@ -2,11 +2,13 @@ package com.storeman.app;
 
 public class MainApp {
 
+
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome Screen");
 		System.out.println("Added new line");
-
+		
+		new GuiViewImpl().initializeView();
 	}
 
 }
