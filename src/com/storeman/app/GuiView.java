c8124c3e764efa0d05b1f3e7efc25a404277d062
Israthnz <zahirul.israth@gmail.com>
2024-10-19 10:05:01 +0530

    Initial commit

---
+++ src/com/storeman/app/GuiView.java
@@ -0,0 +1,6 @@
+package com.storeman.app;
+
+public interface GuiView {
+	public abstract void initializeView();
+	public abstract void loadComponents();
+}
---
+++ src/com/storeman/app/MainApp.java
@@ -0,0 +1,10 @@
+package com.storeman.app;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
